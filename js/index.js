eba11cf0ae9ee56f8ececb6a88da9e9c639980f2
parosky <parosky.g@gmail.com>
2013-05-08 21:09:55 +0900

    modified "円円"

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function(){
     $.getJSON(ajax_url("user"), {key: login_key}, function(data){
         $("h2").text("ユーザ名: "+data.name);
-        $("h3").text("所持金: "+price_int_to_str(data.money)+"円");
+        $("h3").text("所持金: "+price_int_to_str(data.money));
     });
 });
